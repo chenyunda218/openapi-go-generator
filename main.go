b2cc94066bdac537d0716b98e877f858cbc1f7ea
Dan Chen <dan.chen@boardware.com>
2023-11-22 11:59:22 +0800

    feat

--- main.go
+++ main.go
@@ -5,13 +5,19 @@ import (
 	"os"
 
 	model "github.com/chenyunda218/openapi-go-generator/openapi"
+	"github.com/dave/jennifer/jen"
+	"github.com/getkin/kin-openapi/openapi3"
 	"gopkg.in/yaml.v3"
 )
 
+const defaultRoot = "./openapigogenerator"
+const defaultPackageName = "openapigogenerator"
+const defaultInputFile = "./openapi.yaml"
+
 func main() {
-	root := "./openapigogenerator"
-	packageName := "openapigogenerator"
-	inputFile := "./openapi.yaml"
+	root := defaultRoot
+	packageName := defaultPackageName
+	inputFile := defaultInputFile
 	argsWithoutProg := os.Args[1:]
 	for i, v := range argsWithoutProg {
 		if v == "-o" && len(argsWithoutProg) > i+1 {
@@ -34,5 +40,15 @@ func main() {
 	var openapi model.Openapi
 	yaml.Unmarshal(dat, &openapi)
 	openapi.Generate(root, packageName)
-
+	f := jen.NewFile("main")
+	f.Func().Id("main").Params().Block(
+		jen.Qual("fmt", "Println").Call(jen.Lit("Hello, world")),
+	)
+	fmt.Printf("%#v", f)
+	loader := openapi3.NewLoader()
+	doc, err := loader.LoadFromFile(inputFile)
+	for k, v := range doc.Components.Schemas {
+		fmt.Println(k)
+		fmt.Println(v.Value.Type)
+	}
 }
